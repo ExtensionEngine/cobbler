6f60fe8357ce992ee96b2d71754d8e32c5243d89
Luciano Peranni <lucianoperanni95@gmail.com>
2020-06-12 15:05:36 +0200

    Refactor model structure. ADdd endpoint to test model

--- server/user/index.js
+++ server/user/index.js
@@ -4,6 +4,7 @@ const router = require('express').Router();
 const ctrl = require('./user.controller');
 
 router.get('/', ctrl.sayHi);
+router.post('/', ctrl.addUser);
 
 module.exports = {
   path: '/users',
