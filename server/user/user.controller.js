ffcf653c2de6d68b98cacff1cc28f137daa8da83
Luciano Peranni <lucianoperanni95@gmail.com>
2020-06-12 15:05:36 +0200

    Refactor model structure. ADdd endpoint to test model

--- server/user/user.controller.js
+++ server/user/user.controller.js
@@ -1,9 +1,23 @@
 'use strict';
+const User = require('./user.model');
 
 function sayHi(req, res) {
   return res.send('Hi');
 }
 
+function addUser(req, res) {
+  const { firstName, lastName, email, password, role } = req.body;
+  User.create({
+    firstName,
+    lastName,
+    email,
+    password,
+    role
+  })
+  .then(success => res.json(success))
+  .catch(err => res.json('NOPE', err));
+}
+
 module.exports = {
-  sayHi
+  addUser, sayHi
 };
