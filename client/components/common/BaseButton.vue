a5a9512a5dabf784f6c44caed6e4b868195487cc
santicevic <sergijeanticevic@gmail.com>
2020-06-17 15:30:09 +0200

    Fixed common component rename issue

---
+++ client/components/common/BaseButton.vue
@@ -0,0 +1,55 @@
+<template>
+  <button
+    v-on="$listeners"
+    :disabled="disabled"
+    :class="{
+      btn: true,
+      contained,
+      outlined,
+      primary,
+      disabled
+    }">
+    <slot></slot>
+  </button>
+</template>
+
+<script>
+export default {
+  name: 'base-button',
+  props: {
+    disabled: { type: Boolean, default: false },
+    contained: { type: Boolean, default: false },
+    outlined: { type: Boolean, default: false },
+    primary: { type: Boolean, default: false }
+  }
+};
+</script>
+
+<style lang="css" scoped>
+.btn {
+  padding: 5px 15px;
+  outline: none;
+  border-radius: 3px;
+}
+.btn:hover {
+  cursor: pointer;
+}
+.primary {
+  background: var(--color-primary);
+  border: solid 2px var(--color-primary);
+}
+.primary:hover {
+  background: var(--color-primary-500);
+}
+.contained {
+  color: #ffff;
+  border: none;
+}
+.disabled {
+  opacity: 0.4;
+}
+.disabled:hover {
+  cursor: default;
+  opacity: 0.4;
+}
+</style>
---
+++ client/components/common/BaseInput.vue
@@ -0,0 +1,78 @@
+<template>
+  <label class="input-wrapper">
+    <input
+      @input="onChange"
+      :value="value"
+      :type="type"
+      :class="{
+        input: true,
+        filled,
+        outlined
+      }">
+    <span
+      :class="{
+        'input-label': true,
+        'field-filled': value
+      }">
+      {{ label }}
+    </span>
+  </label>
+</template>
+
+<script>
+export default {
+  name: 'base-input',
+  props: {
+    value: { type: String, default: '' },
+    type: { type: String, default: '' },
+    label: { type: String, required: true },
+    filled: { type: Boolean, default: false },
+    outlined: { type: Boolean, default: false }
+  },
+  methods: {
+    onChange(event) {
+      this.$emit('input', event.target.value);
+    }
+  }
+};
+</script>
+
+<style lang="css" scoped>
+.input {
+  font-size: 16px;
+  padding: 12px 16px 14px;
+  border-width: 1px;
+  border-radius: 3px;
+  outline: none;
+  width: calc(100% - 32px);
+}
+.input:hover {
+  border-color: var(--color-neutral-black);
+}
+.filled {
+  border: none;
+  border-bottom: solid 1px var(--color-neutral-black));
+}
+.outlined {
+  border: solid 1px var(--color-neutral-black);
+}
+.input-wrapper {
+  position: relative;
+}
+.input-label {
+  position: absolute;
+  top: 11px;
+  left: 16px;
+  font-size: 16px;
+  transition: all 0.15s ease;
+}
+.input:focus + .input-label,
+.field-filled {
+  font-size: 11px;
+  top: -12px;
+  left: 5px;
+  padding: 2px 7px;
+  background: var(--color-neutral-white);
+  border-radius: 3px 3px 0 0;
+}
+</style>
