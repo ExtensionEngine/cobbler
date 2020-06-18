6fe16274fb145df95f570c9347398a71e7aec56d
Luciano Peranni <lucianoperanni95@gmail.com>
2020-06-18 15:44:25 +0200

    Refactors

--- server/course/course.controller.js
+++ server/course/course.controller.js
@@ -3,7 +3,8 @@ const Course = require('./course.model');
 const pick = require('lodash/pick');
 
 module.exports = {
-  create, getAll
+  create,
+  getAll
 };
 
 function create(req, res) {
@@ -15,6 +16,6 @@ function create(req, res) {
 
 function getAll(req, res) {
   Course.findAll()
-    .then(success => res.json(success))
-    .catch(err => res.json(err));
+    .then(success => res.json({ data: success }))
+    .catch(err => res.status(400).json(err));
 }
