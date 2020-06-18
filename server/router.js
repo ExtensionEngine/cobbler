f7fde2c4eb7d76576574046e27d1653b17b4edc7
Luciano Peranni <lucianoperanni95@gmail.com>
2020-06-18 15:00:29 +0200

    Basic Courses crud

--- server/router.js
+++ server/router.js
@@ -3,12 +3,14 @@
 const auth = require('./auth');
 const { authenticate } = require('./shared/auth');
 const category = require('./category');
+const course = require('./course');
 const router = require('express').Router();
 const user = require('./user');
 
 router.use(auth.path, auth.router);
-router.use(authenticate('jwt'));
+// router.use(authenticate('jwt'));
 router.use(user.path, user.router);
 router.use(category.path, category.router);
+router.use(course.path, course.router);
 
 module.exports = router;
