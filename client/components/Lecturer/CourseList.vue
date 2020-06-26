0b5d506d56eeff035d230b1b35e8490ccc3b4042
santicevic <sergijeanticevic@gmail.com>
2020-06-26 11:04:29 +0200

    Added skeleton loader to lecturer side

--- client/components/Lecturer/CourseList.vue
+++ client/components/Lecturer/CourseList.vue
@@ -1,10 +1,16 @@
 <template>
   <div>
+    <div v-if="loading">
+      <div v-for="n in 5" :key="n" class="course-card"></div>
+    </div>
+    <div v-else-if="!courses.length" class="no-content">There are no courses so far</div>
     <div v-for="course in courses" :key="course.id" class="course-card">
+      <div class="card-info">
+        <h3 class="course-title">{{ course.name }}</h3>
+        <p>{{ course.description }}</p>
+        <span>{{ getDateRange(course) }}</span>
+      </div>
       <span class="category">{{ course.Category.name }}</span>
-      <h3 class="course-title">{{ course.name }}</h3>
-      <p>{{ course.description }}</p>
-      <span>{{ getDateRange(course) }}</span>
     </div>
   </div>
 </template>
@@ -16,7 +22,8 @@ export default {
   name: 'lecturer-course-list',
   data() {
     return ({
-      courses: []
+      courses: [],
+      loading: true
     });
   },
   methods: {
@@ -28,25 +35,80 @@ export default {
     }
   },
   async mounted() {
-    const { data } = await getMyCourses();
-    this.courses = data;
+    try {
+      const { data } = await getMyCourses();
+      this.courses = data;
+      this.loading = false;
+    } catch (err) {
+      // TODO: add toast message in case of error
+      this.loading = false;
+    }
   }
 };
 </script>
 
 <style scoped>
 .category {
-  position: absolute;
-  top: 0;
-  right: 0;
+  align-self: flex-end;
+  flex-basis: 150px;
+  text-align: end;
+}
+.card-info {
+  flex-grow: 1;
 }
 .course-card {
+  display: flex;
   background: var(--color-gray-500);
   border: solid 2px var(--color-black);
   padding: var(--spacing-xs);
   position: relative;
 }
+.course-card:empty {
+  height: 90px;
+  padding: 0
+}
+.course-card:empty:after {
+  content: '';
+  height: 100%;
+  width: 100%;
+  position: absolute;
+background-image: linear-gradient(var(--color-gray), var(--color-gray)),
+    linear-gradient(var(--color-gray), var(--color-gray)),
+    linear-gradient(var(--color-gray), var(--color-gray)),
+    linear-gradient(var(--color-gray), var(--color-gray));
+  background-size: 160px 25px,
+    350px 15px,
+    200px 15px,
+    65px 15px;
+  background-position: 10px 8px,
+    10px 40px,
+    10px 62px,
+    99% 90%;
+  background-repeat: no-repeat;
+  animation: loadingGradient 1.5s infinite;
+}
+.no-content {
+  text-align: center;
+  padding: 15px 0;
+
+}
 .course-title {
   margin: 0;
+}
+@media only screen and (max-width: 480px) {
+  .course-card:empty:after {
+    background-size: 160px 25px,
+    230px 15px,
+    150px 15px,
+    55px 15px;
+  }
+}
+@keyframes loadingGradient {
+  0% {
+    opacity: .3;
+  }
+  100% {
+    opacity: .8;
+  }
 }
 </style>
