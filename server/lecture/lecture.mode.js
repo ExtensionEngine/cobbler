69097f952d8609b76b6cd58365569bb92e4c0223
Sergije Anticevic <sergijeanticevic@gmail.com>
2020-06-26 12:31:24 +0200

    Set up lecture model and migration

---
+++ server/lecture/lecture.mode.js
@@ -0,0 +1,47 @@
+'use strict';
+
+const { Model } = require('sequelize');
+
+class Lecture extends Model {
+  static fields(DataTypes) {
+    const { STRING, TEXT, DATE, INTEGER } = DataTypes;
+    return {
+      name: {
+        type: STRING,
+        validate: { len: [2, 50] }
+      },
+      description: {
+        type: TEXT,
+        validate: { len: [2, 50] }
+      },
+      courseId: {
+        type: INTEGER,
+        field: 'course_id'
+      },
+      createdAt: {
+        type: DATE,
+        field: 'created_at'
+      },
+      updatedAt: {
+        type: DATE,
+        field: 'updated_at'
+      },
+      deletedAt: {
+        type: DATE,
+        field: 'deleted_at'
+      }
+    };
+  }
+
+  static associate({ Course }) {
+    this.belongsTo(Course, { foreignKey: 'courseId' });
+  }
+
+  static options() {
+    return {
+      tableName: 'lectures'
+    };
+  }
+}
+
+module.exports = Lecture;
