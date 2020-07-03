ed84952edde9ee2f01177aae9eb9b6884ec57377
Luciano Peranni <lucianoperanni95@gmail.com>
2020-07-03 14:48:34 +0200

    Re-add deleted components

---
+++ client/components/Course/CategoryFilter.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h1>Filter Categories</h1>
+    <div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'category-filter',
+  data() {
+    return {
+      categories: []
+    };
+  }
+};
+</script>
+
+<style>
+
+</style>
---
+++ client/components/Course/SideBar.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="side-bar-wrapper">
+    <base-search @search="handleSearch" />
+  </div>
+</template>
+
+<script>
+import BaseSearch from '../common/BaseSearch';
+
+export default {
+  name: 'side-bar',
+  components: {
+    BaseSearch
+  }
+};
+</script>
+
+<style>
+  .side-bar-wrapper {
+    height:100vh;
+    background: gray;
+    width: 300px;
+    padding: 10px;
+  }
+</style>
