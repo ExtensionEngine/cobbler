80496d7896b7aa5341880874bb0f5481ad15e9b4
Luciano Peranni <lucianoperanni95@gmail.com>
2020-07-20 12:27:33 +0200

    Fix bug that sends wrong date from date picker

--- client/components/Course/DatePicker.vue
+++ client/components/Course/DatePicker.vue
@@ -3,15 +3,13 @@
     <field
       v-model="date"
       name="start">
-      <template v-slot="{ on, value }">
-        <base-input
-          v-on="on"
-          @input="$emit('input', date)"
-          :value="value"
-          type="date"
-          class="date-picker"
-          outlined />
-      </template>
+      <base-input
+        @input="$emit('input', date)"
+        v-on="$listeners"
+        type="date"
+        v-bind="$attrs"
+        class="date-picker"
+        outlined />
     </field>
   </div>
 </template>
@@ -19,7 +17,6 @@
 <script>
 import BaseInput from '../common/BaseInput';
 import Field from '../common/BaseForm/Field';
-
 export default {
   name: 'date-picker',
   data: () => ({ date: '' }),
