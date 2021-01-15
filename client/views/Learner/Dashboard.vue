2379495748cfaf32157db12ddf99dac959a8bab8
Miro Dojkic <mdojkic@gmail.com>
2021-01-15 21:58:12 +0100

    Enable SSE streaming ✨

--- client/views/Learner/Dashboard.vue
+++ client/views/Learner/Dashboard.vue
@@ -1,37 +1,47 @@
 <template>
   <div>
     <form @submit.prevent="onSubmit">
-      <input v-model="post">
+      <input v-model="text">
       <input type="submit">
     </form>
+    <div v-for="post in posts" :key="post">{{ post }}</div>
   </div>
 </template>
 
 <script>
 import { addPost, subscribe } from '../../api/sse';
+import miss from 'mississippi';
+import mitt from 'mitt';
+import { ReadableWebToNodeStream } from 'readable-web-to-node-stream';
 
 export default {
   name: 'learner-dashboard',
-  data() { return ({ post: '' }); },
+  data: () => ({ posts: [], text: '', emitter: mitt() }),
   methods: {
     async onSubmit() {
       try {
-        await addPost({ post: this.post });
+        await addPost({ post: this.text });
+        this.text = '';
       } catch (error) {
         console.log(error);
       }
+    },
+    emit(buf, enc, next) {
+      const { event, data } = JSON.parse(buf.toString());
+      this.emitter.emit(event, data);
+      next();
     }
   },
   async created() {
-    const readableStream = await subscribe();
-    const decodedStream =
-      await readableStream.data.pipeThrough(new window.TextDecoderStream());
-    const jsonStream = await decodedStream.pipeThrough(new window.TransformStream({
-      transform: (payload, controller) => controller.enqueue(JSON.parse(payload))
-    }));
-    console.log(await jsonStream.getReader().read());
-    // const writableStream = await jsonStream.pipeTo(new WritableStream());
-    // console.log(writableStream);
+    const response = await subscribe();
+    const sse = new ReadableWebToNodeStream(response.data);
+    miss.pipe(sse, miss.through.obj(this.emit));
+    this.emitter.on('CONNECTED', () => {
+      console.log('Connected');
+    });
+    this.emitter.on('POST_CREATED', ({ post }) => {
+      this.posts.push(post);
+    });
   }
 };
 </script>
--- server/sse/sse.controller.js
+++ server/sse/sse.controller.js
@@ -1,34 +1,34 @@
 'use strict';
 
+const miss = require('mississippi');
+
 module.exports = {
   subscribe,
   addPost
 };
 
-let clients = [];
+const subscribers = {};
 const posts = [];
 
 function subscribe(req, res) {
-  res.writeHead(200, {
-    'Content-Type': 'text/event-stream',
-    Connection: 'keep-alive',
-    'Cache-Control': 'no-cache'
-  });
-  res.write(`${JSON.stringify(posts)}\n\n`);
-  clients.push(req.user);
-  req.on('close', () => {
-    console.log(`${req.user.id} Connection closed`);
-    clients = clients.filter(c => c.id !== req.user.id);
+  res.writeHead(200, { 'Content-Type': 'text/event-stream' });
+  subscribers[req.user.id] = res;
+  res.write(JSON.stringify({ event: 'CONNECTED' }));
+  miss.finished(req, () => {
+    delete subscribers[req.user.id];
   });
 }
 
-function sendEventsToAll(newPost) {
-  clients.forEach(c => c.res.write(`${JSON.stringify(newPost)}\n\n`));
+function broadcast(message) {
+  Object.values(subscribers).forEach(sub => {
+    sub.write(`${message}\n`);
+  });
 }
 
 async function addPost(req, res) {
   const newPost = req.body;
   posts.push(newPost);
-  res.json({ data: newPost });
-  return sendEventsToAll(newPost);
+  const message = JSON.stringify({ event: 'POST_CREATED', data: newPost });
+  broadcast(message);
+  return res.json(message);
 }
